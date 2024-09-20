15c5ed1984f0353e2aeb62f82599f8d5ff3218c2
Daniel Bowers <34407755+DanBowersJR@users.noreply.github.com>
2024-09-19 22:36:35 -0230

    Add feature: Include uppercase letters in the password

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 // Default settings
 let length = 8;
+let useNumbers = false;
+let useUppercase = false;
 
 // Function to display the help menu
 const showHelp = () => {
@@ -11,9 +13,11 @@ const showHelp = () => {
 
     Options:
       --help, -h           Show help menu
+      --numbers, -n        Include numbers in the password
+      --uppercase, -u      Include uppercase letters in the password
 
     Example:
-      node index.js        # Generate a basic password (no features)
+      node index.js --numbers --uppercase   # Generate a password with numbers and uppercase letters
     `);
     process.exit(0);
 };
@@ -27,10 +31,46 @@ for (let i = 0; i < args.length; i++) {
         case '-h':
             showHelp();
             break;
+        case '--numbers':
+        case '-n':
+            useNumbers = true;
+            break;
+        case '--uppercase':
+        case '-u':
+            useUppercase = true;
+            break;
         default:
             console.error(`Error: Unknown option '${arg}'`);
             showHelp();
     }
 }
 
-console.log('Password Generator is running with default settings (length = 8)');
+// Function to generate a password
+const generatePassword = (length, useNumbers, useUppercase) => {
+    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '0123456789';
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    let characterPool = lowercase;
+
+    if (useNumbers) {
+        characterPool += numbers;
+    }
+
+    if (useUppercase) {
+        characterPool += uppercase;
+    }
+
+    // Generate the password
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characterPool.length);
+        password += characterPool[randomIndex];
+    }
+
+    return password;
+};
+
+// Generate and display the password
+const password = generatePassword(length, useNumbers, useUppercase);
+console.log(`Generated Password: ${password}`);
